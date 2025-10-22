7178cc87cf99d5ad3f4723c98a5e926064cac425
ChefStefTMU <stefan.aquino@torontomu.ca>
2025-10-22 12:23:27 -0400

    Pre Lab Files

---
+++ Reaction_Wheel_Model_Variables.m
@@ -0,0 +1,10 @@
+%% Reaction Wheel Model Variables %%
+R = 5.3; %Ohms
+K = 0.022 %Nm/A
+Jm = 1.4*10^-6 %Kg*m^2
+L = 580*10^-6 %H
+Js = 0.0022 %kg*m^2
+Jdisk = 1.0323 %kg*m^2
+J = Jm + Jdisk
+omegas = 209.44
+Kta = 1
